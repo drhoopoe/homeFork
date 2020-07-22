e453af05668bc65f71705e9485a4956b7d3af2a1
drhoopoe <noah.d.gardiner@gmail.com>
2020-07-22 02:09:35 -0400

    homeFork personalization

--- script/script.js
+++ script/script.js
@@ -1,36 +1,38 @@
 const Config = {
-    name: "user",
+    name: "noah",
     scale: 1,
     Links: [
         [
-            "site",
+            "hoopoe",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["thehoopoe.org", "https://thehoopoe.org/"],
+                ["netlify", "https://app.netlify.com/sites/the-hoopoe/overview"]
+                ["github", "https://github.com/drhoopoe/academic-kickstart"]
             ]
         ],
         [
-            "site",
+            "research",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["ejtaal", "http://ejtaal.net/aa/#hw4=14,ll=38,ls=1,la=1,sg=20,ha=21,br=26,pr=9,aan=24,mgf=33,vi=51,kz=10,mr=25,mn=1,uqw=106,umr=26,ums=14,umj=34,ulq=247,uqa=17,uqq=2,bdw=h19,amr=h7,asb=h17,auh=h37,dhq=h2,mht=h6,msb=h8,tla=h8,amj=h22,ens=h1,mis=h1"],
+                ["academia", "https://sc.academia.edu/NoahGardiner/Analytics/activity/overview"]
+                ["dbox", "https://www.dropbox.com/home"]
             ]
         ],
         [
-            "site",
+            "teaching",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["blackboard", "https://blackboard.sc.edu/"],
+                ["class files", "https://emailsc-my.sharepoint.com/personal/gardinen_mailbox_sc_edu/_layouts/15/onedrive.aspx"],
+                ["my.sc", "https://my.sc.edu/"]
             ]
         ],
         [
-            "site",
+            "other",
             [
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"],
-                ["link", "https://www.example.com"]
+                ["feeds", "https://www.inoreader.com/all_articles"],
+                ["nts", "https://www.nts.live/"],
+                ["bandcamp", "https://bandcamp.com/ngard1035"],
+                ["netflix", "https://www.example.com"]
             ]
         ]
     ]
