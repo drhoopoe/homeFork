93dc043d9068d2597663d1742df7ee4911e04d47
Noah Gardiner <65883669+drhoopoe@users.noreply.github.com>
2020-07-22 02:34:21 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ const Config = {
             "hoopoe",
             [
                 ["thehoopoe.org", "https://thehoopoe.org/"],
-                ["netlify", "https://app.netlify.com/sites/the-hoopoe/overview"]
+                ["netlify", "https://app.netlify.com/sites/the-hoopoe/overview"],
                 ["github", "https://github.com/drhoopoe/academic-kickstart"]
             ]
         ],
@@ -14,7 +14,7 @@ const Config = {
             "research",
             [
                 ["ejtaal", "http://ejtaal.net/aa/#hw4=14,ll=38,ls=1,la=1,sg=20,ha=21,br=26,pr=9,aan=24,mgf=33,vi=51,kz=10,mr=25,mn=1,uqw=106,umr=26,ums=14,umj=34,ulq=247,uqa=17,uqq=2,bdw=h19,amr=h7,asb=h17,auh=h37,dhq=h2,mht=h6,msb=h8,tla=h8,amj=h22,ens=h1,mis=h1"],
-                ["academia", "https://sc.academia.edu/NoahGardiner/Analytics/activity/overview"]
+                ["academia", "https://sc.academia.edu/NoahGardiner/Analytics/activity/overview"],
                 ["dbox", "https://www.dropbox.com/home"]
             ]
         ],
